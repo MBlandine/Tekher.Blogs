930d6f22d91903dde751b786b2ed547147d2adbb
BlandineM <blandinemunezero8@gmail.com>
2025-02-07 13:51:34 +0200

    layout

--- src/componets/Layout.jsx
+++ src/componets/Layout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import Footer from './Footer'
+import Footer from './Footer';
 import Navbar from "./Navbar";
-import '../styles.css/layout.css';
 import {Outlet} from 'react-router-dom';
  function Layout (){
     return(
